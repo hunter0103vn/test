184bf358b2e6bf156467610133d1db4b6450e46b
hunter0103vn <hunter0103vn@yahoo.com>
2017-06-24 05:43:37 +0700

    Create mind.js

---
+++ mind.js
@@ -0,0 +1,170 @@
+var _81ad6362418182 = {"_31":[
+	{ key: "infinitive", color: "lightgray", description: "" },
+	{ key: "imperative", color: "lightgray", description: "" },
+	{ key: "active-participle", color: "lightgray", description: "" },
+	{ key: "gerund", color: "lightgray", description: "" },
+	{ key: "s-form(3rd-person)", color: "lightgreen", description: "" },
+	{ key: "ing-form", color: "lightgreen", description: "" },
+	{ key: "past-form", color: "lightgreen", description: "" },
+	{ key: "p2-form", color: "lightgreen", description: "" },
+	{ key: "base-form", color: "lightgreen", description: "" },
+	{ key: "VERB", color: "lightblue", description: "" },
+	{ key: "structure", color: "pink", description: "" },
+	{ key: "modal-have-be(continuous)-be(p2)-V", color: "lightgray", description: "will steal" },
+	{ key: "aspect", color: "orange", description: "" },
+	{ key: "finite?", color: "red", description: "" },
+	{ key: "tense", color: "orange", description: "" },
+	{ key: "modal?", color: "orange", description: "" },
+	{ key: "voice", color: "orange", description: "" },
+	{ key: "perfect?", color: "lightgray", description: "" },
+	{ key: "continuous?", color: "lightgray", description: "" },
+	{ key: "present", color: "lightgray", description: "" },
+	{ key: "past", color: "lightgray", description: "" },
+	{ key: "passive", color: "lightgray", description: "" },
+	{ key: "active", color: "lightgray", description: "" }
+],"_23":[
+	{ from: "VERB", to: "s-form(3rd-person)" },
+	{ from: "VERB", to: "ing-form" },
+	{ from: "VERB", to: "past-form" },
+	{ from: "VERB", to: "p2-form" },
+	{ from: "VERB", to: "base-form" },
+	{ from: "VERB", to: "structure" },
+	{ from: "VERB", to: "aspect" },
+	{ from: "VERB", to: "finite?" },
+	{ from: "VERB", to: "tense" },
+	{ from: "VERB", to: "modal?" },
+	{ from: "VERB", to: "voice" },
+	{ from: "base-form", to: "imperative" },
+	{ from: "base-form", to: "infinitive" },
+	{ from: "structure", to: "modal-have-be(continuous)-be(p2)-V" },
+	{ from: "aspect", to: "perfect?" },
+	{ from: "aspect", to: "continuous?" },
+	{ from: "tense", to: "present" },
+	{ from: "tense", to: "past" },
+	{ from: "past-form", to: "past" },
+	{ from: "base-form", to: "present" },
+	{ from: "s-form(3rd-person)", to: "present" },
+	{ from: "voice", to: "passive" },
+	{ from: "voice", to: "active" },
+	{ from: "ing-form", to: "active-participle" },
+	{ from: "ing-form", to: "gerund" }
+]};
+
+var _81ada62428182 = {"_31":[
+	{ key: "NOUN", color: "lightblue", description: "<b>Security guards</b> set a trap, <br> The stewardess alerted <b>the pilot</b>, <br> The cost of a bottle was <b>17 pence</b>, <br> <b>That day</b> something unusual happened, <br> The passengers left in <b>a hurry</b> through <b>fire exits</b>" },
+	{ key: "physical-things", color: "orange", description: "aircraft, clock, door, whisky" },
+	{ key: "ideas-quanlities", color: "orange", description: "time, result, security" },
+	{ key: "actions-events", color: "orange", description: "theft, landing" },
+	{ key: "roles", color: "orange", description: "pilot, spokesman" },
+	{ key: "name", color: "orange", description: "Berlin" },
+	{ key: "move-ment", color: "lightgray", description: "" },
+	{ key: "inten-tion", color: "lightgray", description: "" },
+	{ key: "differen-ce", color: "lightgray", description: "" },
+	{ key: "kind-ness", color: "lightgray", description: "" },
+	{ key: "secur-ity", color: "lightgray", description: "" },
+	{ key: "land-ing", color: "lightgray", description: "" },
+	{ key: "gender?", color: "red", description: "steward/stewardess, actor/actress" },
+	{ key: "structure", color: "pink", description: "" },
+	{ key: "quantifier-determiner-adj.modifier-n.modifier-N-other.modifier", color: "lightgray", description: "... a ... ... bomb ...,<br> ... a hot ... meal for two,<br>... the ... ... door, <br> all these ... ... bottles here, <br> a lot of ... empty ... bottles ..., <br> a lot of her ... ... friends ..., <br> enough ... ... ... exits ..., <br> some ... nice soup dishes ..., <br> each of the heavy glass doors of the building" },
+	{ key: "quantifiers", color: "lightgreen", description: "a lot of, many, much, a few, every, each, all, most, both, half, some, any, one, two, three..." },
+	{ key: "determiners", color: "lightgreen", description: "" },
+	{ key: "modifiers", color: "lightgreen", description: "" },
+	{ key: "articles", color: "lightgray", description: "a, an, the" },
+	{ key: "demonstratives", color: "lightgray", description: "this, that, these, those" },
+	{ key: "possessives", color: "lightgray", description: "my, her, your" },
+	{ key: "adjective-phrase", color: "lightgray", description: "" },
+	{ key: "noun-phrase", color: "lightgray", description: "" },
+	{ key: "preposition-phrase", color: "lightgray", description: "" },
+	{ key: "adverb-phrase", color: "lightgray", description: "" },
+	{ key: "clause", color: "lightgray", description: "" },
+],"_23":[
+	{ from: "NOUN", to: "physical-things" },
+	{ from: "NOUN", to: "ideas-quanlities" },
+	{ from: "NOUN", to: "actions-events" },
+	{ from: "NOUN", to: "roles" },
+	{ from: "NOUN", to: "name" },
+	{ from: "NOUN", to: "move-ment" },
+	{ from: "NOUN", to: "inten-tion" },
+	{ from: "NOUN", to: "kind-ness" },
+	{ from: "NOUN", to: "differen-ce" },
+	{ from: "NOUN", to: "secur-ity" },
+	{ from: "NOUN", to: "land-ing" },
+	{ from: "NOUN", to: "gender?" },
+	{ from: "NOUN", to: "structure" },
+	{ from: "NOUN", to: "quantifiers" },
+	{ from: "NOUN", to: "determiners" },
+	{ from: "NOUN", to: "modifiers" },
+	{ from: "structure", to: "quantifier-determiner-adj.modifier-n.modifier-N-other.modifier" },
+	{ from: "determiners", to: "articles" },
+	{ from: "determiners", to: "demonstratives" },
+	{ from: "determiners", to: "possessives" },
+	{ from: "modifiers", to: "adjective-phrase" },
+	{ from: "modifiers", to: "noun-phrase" },
+	{ from: "modifiers", to: "preposition-phrase" },
+	{ from: "modifiers", to: "adverb-phrase" },
+	{ from: "modifiers", to: "clause" },
+	{ from: "NOUN", to: "noun-phrase" }
+]};
+
+var _81a414363438182 = {"_31":[
+	{ key: "PREPOSITION", color: "lightblue" }
+],"_23":[
+	{ from: "PREPOSITION", to: "PREPOSITION" }
+]};
+
+var _81aca82c82 = {"_31":[
+	{ key: "SENTENCE", color: "blue" },
+	{ key: "Noun", color: "lightblue" },
+	{ key: "Verb", color: "lightblue" },
+	{ key: "Clause", color: "lightblue" },
+	{ key: "Conjunction", color: "lightblue" }
+],"_23":[
+	{ from: "SENTENCE", to: "Noun" },
+	{ from: "SENTENCE", to: "Verb" },
+	{ from: "SENTENCE", to: "Clause" },
+	{ from: "SENTENCE", to: "Conjunction" }
+]};
+
+$(document).ready(function() {
+	var _51744163$ = go.GraphObject.make;
+	var myDiagram = _51744163$(go.Diagram, "divB", {
+		initialContentAlignment: go.Spot.Center, // center Diagram contents
+		"undoManager.isEnabled": true // enable Ctrl-Z to undo and Ctrl-Y to redo
+	});
+
+	function showMessage(s) {
+		document.getElementById("divC").innerHTML = s;
+	}
+
+	myDiagram.addDiagramListener("ObjectSingleClicked", function(e) {
+		var part = e.subject.part;
+		if (!(part instanceof go.Link)) showMessage(part.data.description);
+	});
+
+	myDiagram.nodeTemplate = _51744163$(go.Node, "Auto",  // the Shape will go around the TextBlock
+		_51744163$(go.Shape, "RoundedRectangle", { strokeWidth: 0},
+			// Shape.fill is bound to Node.data.color
+			new go.Binding("fill", "color")),
+		_51744163$(go.TextBlock,
+		  { margin: 8 },  // some room around the text
+			// TextBlock.text is bound to Node.data.key
+			new go.Binding("text", "key"))
+	);
+
+	function layout(_31, _23, _34, _74) {
+		_34.model = new go.GraphLinksModel(_31, _23);
+		_34.startTransaction();
+		var clay = new go.CircularLayout();
+		clay.doLayout(_34);
+		var root = _34.model.findNodeDataForKey(_74);
+		if (root !== null) root.location = clay.actualCenter;
+		var flay = new go.ForceDirectedLayout();
+		flay.doLayout(_34);
+		_34.commitTransaction("layout");
+	}
+
+	// layout(_81ad6362418182["_31"], _81ad6362418182["_23"], myDiagram, "VERB");
+	layout(_81ada62428182["_31"], _81ada62428182["_23"], myDiagram, "NOUN");
+	// layout(_81a414363438182["_31"], _81a414363438182["_23"], myDiagram, "PREPOSITION");
+	// layout(_81aca82c82["_31"], _81aca82c82["_23"], myDiagram, "SENTENCE");
+});
